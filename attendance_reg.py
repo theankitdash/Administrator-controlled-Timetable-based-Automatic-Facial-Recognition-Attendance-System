68efbc673fe91b0ba8a63c51db3e7bbb939c5ee4
Ankit Dash <ankitdash3037@gmail.com>
2022-11-11 23:48:26 +0530

    Commit

--- attendance_reg.py
+++ attendance_reg.py
@@ -6,3 +6,17 @@ import mysql.connector
 import cv2
 import os
 import numpy as np
+
+class Attendance_Registration:
+    def __init__(self, root):
+        self.root=root
+        self.root.geometry("500x700+0+0")
+        self.root.title("Attendance Registration")
+
+               
+
+
+if __name__ == "__main__":
+    root=Tk()  
+    obj=Attendance_Registration(root)
+    root.mainloop()                
