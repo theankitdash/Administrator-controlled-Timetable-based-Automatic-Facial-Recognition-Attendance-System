60f3e2464e7430baa3496f0af33b1ea2532b83e1
Ankit Dash <ankitdash3037@gmail.com>
2023-03-06 20:03:24 +0530

    Changes commit

--- test.py
+++ test.py
@@ -1,53 +1,25 @@
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
-from tkinter import messagebox
-import mysql.connector
 import cv2
 import os
-import csv
 import numpy as np
-import pandas as pd
 import time
 import datetime
-from glob import glob
 import schedule
 
 trainedimages = ("classifier.xml")
-harcascadePath = "haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_eye.xml')
 attendance_path = "C:\\Users\\ankit\\Desktop\\New folder\\Subjects"
 
 class Attendance_Registration:
-    def __init__(self, root):
-        self.root=root
-        self.root.geometry("1280x720+0+0")
-        self.root.title("Attendance Registration")
+    def period1(self):
+        conn = mysql.connector.connect(host='localhost', username='root', password='Chiku@3037', database='attendance-system', charset='utf8')
+        my_cursor = conn.cursor()
+        cursor.execute("select SUBCODE from schedule where ID='B11'")
+        sub = cursor.fetchone()
 
-        self.var_Sub=StringVar()
+        subject = "".join(map(str, sub))
+        schedule.every().day.at("09:00").do(FillAttendance)
 
-        #Bg Image
-        img=Image.open(r"C:\Users\ankit\Desktop\New folder\itersoa.jpg")
-        img=img.resize((1280,720),Image.ANTIALIAS)
-        self.photoimg=ImageTk.PhotoImage(img)
-
-        BgImage = Label(self.root, image=self.photoimg)
-        BgImage.place(x=0,y=0,width=1280,height=720)
-
-        title = Label(BgImage, text="ATTENDANCE REGISTRATION", font=("Times new roman", 25,"bold"),bg="white")
-        title.place(x=0,y=0,width=1280,height=45)
-
-        sub_combo=ttk.Combobox(BgImage, textvariable=self.var_Sub, font=("times new roman",15,"bold"), state="readonly")
-        sub_combo["values"]=("Select Subject", "PSAD-2", "UHV", "ESPUA", "PSH", "PSM", "ECES")
-        sub_combo.current(0)
-        sub_combo.place(x=500,y=200,width=250,height=50)
-
-        bt=Button(BgImage,text="RECOGNIZE",command=self.FillAttendance,cursor="hand2",font=("times new roman",18,"bold"),bg="darkgreen",fg="white")
-        bt.place(x=500,y=270,width=250,height=50)
-
-        bt1=Button(BgImage,text="VIEW ATTENDANCE",command=self.calculate_attendance,cursor="hand2",font=("times new roman",18,"bold"),bg="darkgreen",fg="white")
-        bt1.place(x=500,y=350,width=250,height=50)
-
-    
     def FillAttendance(self):
         sub = self.var_Sub.get()
         now = time.time()
@@ -88,7 +60,7 @@ class Attendance_Registration:
                 i=my_cursor.fetchone()
                 i="+".join(i)
                         
-                if confidence>50:
+                if confidence>60:
                     global Subject
                     global aa
                     global date
@@ -131,73 +103,26 @@ class Attendance_Registration:
         date = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d")
         timeStamp = datetime.datetime.fromtimestamp(ts).strftime("%H:%M:%S")
         Hour, Minute, Second = timeStamp.split(":")
-        
-        path = os.path.join(attendance_path, Subject)
-        fileName = (
-            f"{path}/"
-            + Subject
-            + "_"
-            + date
-            + "_"
-            + Hour
-            + "-"
-            + Minute
-            + "-"
-            + Second
-            + ".csv"
-        )
-        attendance = attendance.drop_duplicates(["Roll No"], keep="first")
-        print(attendance)
-        attendance.to_csv(fileName, index=False)
 
-        messagebox.showinfo("Congrats","Attendance marked successfully of " + Subject)
+        str_id = str(Id)
+        path = os.path.join(attendance_path, str_id)
+        isExist = os.path.exists(path)
         
+        def save_attendance():    
+            fileName = (f"{path}/"+ str_id+ "_" + date+ "_" + Hour + "-" + Minute + "-" + Second + ".csv")
+            attendance1 = attendance.drop_duplicates(["Roll No"], keep="first")
+            print(attendance1)
+            attendance1.to_csv(fileName, index=False)
+
+        if isExist:
+            save_attendance()
+        else:
+            os.mkdir(path) 
+            save_attendance()
+
+
         cam.release()
         cv2.destroyAllWindows()
-
-
-    def calculate_attendance(self):
-        Subject = self.var_Sub.get()
-        if Subject=="":
-            message.showinfo('Please enter the subject name.')
-            
-        os.chdir(
-            f"C:\\Users\\ankit\\Desktop\\New folder\\Subjects\\{Subject}"
-        )
-        filenames = glob(
-            f"C:\\Users\\ankit\\Desktop\\New folder\\Subjects\\{Subject}\\{Subject}*.csv"
-        )
-
-        df = [pd.read_csv(f) for f in filenames]
-        newdf = df[0]
-        for i in range(1, len(df)):
-            newdf = newdf.merge(df[i], how="outer")
-        newdf.fillna(0, inplace=True)
-
-        newdf["Attendance"] = 0
-        for i in range(len(newdf)):
-            newdf["Attendance"].iloc[i] = str(int(round(newdf.iloc[i, 2:-1].mean() * 100)))+'%'
-            
-        newdf.to_csv("attendance.csv", index=False)
-
-        root = Tk()
-        root.title("Attendance of "+Subject)
-        root.configure(background="black")
-        cs = f"C:\\Users\\ankit\\Desktop\\New folder\\Subjects\\{Subject}\\attendance.csv"
-        with open(cs) as file:
-            reader = csv.reader(file)
-            r = 0
-
-            for col in reader:
-                c = 0
-                for row in col:
-
-                    label = Label(root, width=10, height=1, fg="black", font=("times", 15, " bold "), bg="white",text=row, relief=RIDGE)
-                    label.grid(row=r, column=c)
-                    c += 1
-                r += 1
-        root.mainloop()
-        print(newdf) 
     
 if __name__ == "__main__":
     root=Tk()  
